43af5dc2280415900ec0baab3b410e9848d11635
Daniel Watson <55237431+ddddeano@users.noreply.github.com>
2021-06-15 11:41:18 +0200

    Add files via upload

---
+++ ContentView.swift
@@ -0,0 +1,44 @@
+//
+//  ContentView.swift
+//  AddNewItemSheet
+//
+//  Created by Daniel Watson on 15/06/2021.
+//
+
+import SwiftUI
+
+class ContentViewModel: ObservableObject {
+    
+    @Published var showAddNewIncomeSheet = false
+}
+
+struct ContentView: View {
+    
+    @StateObject var VM = ContentViewModel()
+    
+    var body: some View {
+        NavigationView {
+            List {
+                Text("Hello, world!")
+                    .padding()
+                
+            }
+            .navigationBarItems(
+                leading: Text("Items"),
+                trailing: Image(systemName: "plus.circle"))
+        }
+    }
+}
+
+struct AddNewIncomeSheet: View {
+    var body: some View {
+        Text("Im a new Income Sheet")
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
